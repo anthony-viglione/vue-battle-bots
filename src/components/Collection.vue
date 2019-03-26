45d6f02807e24c175b16886769ab7efe8c8b8939
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-26 15:26:25 -0600

    added basic fields for project

--- src/components/Collection.vue
+++ src/components/Collection.vue
@@ -1,6 +1,16 @@
 <template>
 <div>
     <h1>COLLECTION</h1>
+    <p>Bot #1: Select a bot below</p>
+    <p>Bot #2: Select a bot below</p>
+    <div>
+        <button>Battle</button>
+        <button>Clear</button>
+    </div>
+    <hr>
+    <div>
+        Bot Bot Bot Bot
+    </div>
 </div>
 </template>
 
