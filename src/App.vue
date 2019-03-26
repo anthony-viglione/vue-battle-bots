68ca5868bddcaabd4cda046523d9d5f0420c6955
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-26 14:52:59 -0600

    created app, removed default component import, added import for Create and Collection components, added basic header(top) with display flex

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="top">
+      <button>CREATE</button>
+      <section>BATTLE BOTS</section>
+      <button>COLLECTION</button>
+      <hr>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Create from './components/Create.vue';
+import Collection from './components/Collection';
 
 export default {
   name: 'app',
+  data() {
+
+  },
   components: {
-    HelloWorld
+    Create,
+    Collection
   }
 }
 </script>
@@ -24,5 +33,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#top{
+  display: flex;
 }
 </style>
