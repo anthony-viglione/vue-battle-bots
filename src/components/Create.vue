d2e8d4a390685b360cde94cd678b2987a0d76166
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-26 15:26:41 -0600

    added basic fields for project

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,5 +1,16 @@
 <template>
+<div>
     <h1>CREATE</h1>
+    <div>
+        <input type="text">
+        <input type="text">
+        <input type="text">
+        <div>
+            <button>SUBMIT</button>
+            <button>CLEAR</button>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
